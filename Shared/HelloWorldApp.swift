5ebf1386e546c29d35949c54966811f6de7ad7f4
Aleksey Zakharenko <zaharenko.alex@gmail.com>
2021-02-21 11:35:53 +0300

    Initial Commit

---
+++ Shared/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  Shared
+//
+//  Created by Aleksey Zakharenko on 21.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
